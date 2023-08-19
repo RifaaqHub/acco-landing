18f54ebc10d9fc31b106715ad33a8c9e74daaf84
reedwane <abdulkareemtemmy07@gmail.com>
2023-08-19 18:45:05 +0100

    update

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -7,7 +7,7 @@ interface HeadingProps {
 }
 
 const headingVariants: Record<HeadingVariant, TwStyle> = {
-  h1: tw`text-[2.5rem] leading-[3.25rem] md:(text-[3.4rem] leading-[4rem]) lg:(text-[4rem] leading-[5rem]) xl:(text-[4.5rem] leading-[6.6rem])`,
+  h1: tw`text-[2.5rem] leading-[2.75rem] md:(text-[3rem] leading-[4rem]) lg:(text-[3.5rem] leading-[5rem]) xl:(text-[4.5rem] leading-[6.6rem])`,
   h2: tw`text-[2.5rem] leading-[3.25rem] md:(text-[3rem] leading-[3.5rem]) lg:(text-[3.5rem] leading-[4rem]) xl:(text-[4rem] leading-[5rem])`,
   h3: tw`text-[1.75rem] leading-[2.275rem] md:(text-[1.85rem] leading-[2.4rem]) lg:(text-[2rem] leading-[2.75rem]) xl:(text-[2.25rem] leading-[2.925rem])`,
   h4: tw`text-[1.25rem] leading-[1.75rem] md:(text-[1.85rem] leading-[2.4rem]) xl:(text-[2rem] leading-[2.75rem])`,
