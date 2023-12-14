3378576b80a99c1a84043dae68999ee98da0aaf0
reedwane <abdulkareemtemmy07@gmail.com>
2023-12-14 18:34:36 +0100

    fix flex gap on cta bbanner

--- src/app/(home)/home/Attention.tsx
+++ src/app/(home)/home/Attention.tsx
@@ -3,7 +3,7 @@ import { GetStarted, Heading, Maxwidth, Paragraph } from '@/components'
 const Attention = () => {
   return (
     <section tw="bg-lightGold px-6 py-12 lg:(px-[6.25rem] py-[3.75rem])">
-      <Maxwidth tw="p-6 flex flex-col items-center bg-primary text-white rounded-[1rem] shadow-2xl lg:(py-16 flex-row px-[6.25rem])">
+      <Maxwidth tw="p-6 flex flex-col items-center bg-primary text-white rounded-[1rem] gap-6 shadow-2xl lg:(py-16 flex-row px-[6.25rem])">
         <div tw="flex-1">
           <Heading as={'h4'} $variant="h4" tw="pb-4">
             Start Your Journey Today!
